54ba8bad0c048b232f041df03aa32dd9f51a3c2a
Daedra <foglzerika@gmail.com>
2020-03-20 05:21:28 -0600

    ref | Adding .env files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,Fragment} from 'react';
 
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
@@ -8,22 +8,44 @@ import Nav from "./components/nav/Nav";
 import SignUp from './components/auth/Signup'
 import CoinsList from "./components/coinsList/CoinsList";
 import CoinDetail from "./components/coinDetail/CoinDetail";
-function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Nav/>
-          <Switch>
-              <Route exact path="/" component={CoinsList}/>
-              <Route exact path="/signup" component={SignUp}/>
-              <Route exact path="/:id" component={CoinDetail}/>
-          </Switch>
-        </div>
+import AuthService from "./components/auth/auth-service";
+import MyContext from "./context";
 
-      </Router>
+const App = props => {
+    const [user , setUser] = useState(null);
+    const service = new AuthService();
+
+    const fetchUser = () =>{
+        if(user === null){
+            service.loggedin()
+                .then(response =>{
+                    setUser(response)
+                })
+                .catch( e => setUser(null))
+        }
+    };
+
+
+  return (
+      <MyContext.Provider value={{user: user , updateContext: setUser}}>
+          <Router>
+            <div className="App">
+              <Nav/>
+              <Switch>
+                  <Fragment>
+                      <Route exact path="/signup" component={SignUp}/>
+                  </Fragment>
+                  <Route exact path="/" component={CoinsList}/>
+
+                  <Route exact path="/:id" component={CoinDetail}/>
+              </Switch>
+            </div>
+
+          </Router>
+      </MyContext.Provider>
 
 );
-}
+};
 
 
 export default App;
--- src/components/auth/auth-service.js
+++ src/components/auth/auth-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class AuthService{
     constructor(){
         let service = axios.create({
-            baseURL : 'https://crypo-wallet-server.herokuapp.com/api',
+            baseURL : process.env.API_URL ,
             withCredentials: true
         });
         this.service = service;
@@ -12,7 +12,13 @@ class AuthService{
     signup = (username, password) => {
         return this.service.post('/signup', {username, password})
             .then(response => response.data)
+    };
+
+    loggedin = () => {
+        return this.service.get('/loggedin')
+            .then(response => response.data)
     }
+
 
 }
 export default AuthService
